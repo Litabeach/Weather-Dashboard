d04c14a7e803b3aef78087cc7514e343226e44f9
Lita Beach <litabeach123@gmail.com>
2021-01-22 13:20:17 -0600

    city buttons partial work

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,25 @@
+
+inputDiv = $("enter-city")
+
+var cities = [""];
+
+function renderCities(){
+    $("#cities-buttons").empty();
+    for (var i=0; i < cities.length; i ++){
+        var button = $("<button>");
+        button.attr("class", "city");
+        button.attr("data-name", cities[i]);
+        button.text(cities[i]);
+        $("#cities-buttons").append(button)
+    }
+}
+
+
+
 $("#search").on("click", function (event) {
     event.preventDefault();
-    
+    var city = $("#enter-city").val().trim();
+    cities.push(city);
+    renderCities();
 });
+
