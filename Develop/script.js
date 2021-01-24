bbee85b7f4b9a870e2fd03fdd555c844b4d9d873
Lita Beach <litabeach123@gmail.com>
2021-01-24 16:46:17 -0600

    API working, three components

--- Develop/script.js
+++ Develop/script.js
@@ -10,40 +10,37 @@ function renderCities() {
         button.attr("class", "city");
         button.attr("data-name", cities[i]);
         button.text(cities[i]);
-        $("#cities-buttons").append(button)
+        button.click(() => {
+            showOneDay(cities[i]);
+            // showFiveDay(cities[i])
+        });
+        $("#cities-buttons").append(button);
     }
 }
 
-function showOneDay() {
-    var thisButton = $(this);
-    //var thisMovieName = thisButton.attr("data-name");
-    var thisCityName = thisButton.attr("data-name");
-
+function showOneDay(city) {
     // URL with API Key
-    var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + thisCityName + "&appid=ec56ab5d5bd61e4c7ba5abb45ce5cb1a";
-  
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ec56ab5d5bd61e4c7ba5abb45ce5cb1a";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     })
         .then(function (response) {
-            //   in here will be the info we want to pull about weather data
-            console.log(response)
-            var weatherCity = response.city.name;
-            var weatherDate = response.list.dt_txt;
-            var weatherIcon = response.list.weather.icon;
+            var weatherCity = response.name;
+            var weatherDate = new Date(response.dt * 1000).toString();
+            var weatherIcon = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
             // var weatherTemp = response.list.main.temp;
             // var weatherHumidity = response.list.main.humidity;
             // var weatherWindspeed = response.list.wind.speed;
             // var weatherUVIndex = response.???? will need to use lat and lon  response.city.coord.lat, .lon
-            
+
             // one day weather div
             var oneDayDiv = $("<div class='oneDay'>");
 
             // city
             var weatherCityEl = $("<h3>");
-            oneCityEl.html(weatherCity);
+            weatherCityEl.html(weatherCity);
 
             // date
             var weatherDateEl = $("<p>");
@@ -51,11 +48,14 @@ function showOneDay() {
 
             // icon
             var weatherIconEl = $("<img>");
-            weatherIcon.attr("src", weatherIcon);
+            weatherIconEl.attr("src", weatherIcon);
+            
 
-            weatherCityEl.append(weatherDateEl);
-            weatherCityEl.append(weatherIconEl);
-        
+            // weatherCityEl.append(weatherIconEl);
+            oneDayDiv.append(weatherCityEl);
+            oneDayDiv.append(weatherDateEl);
+            oneDayDiv.append(weatherIconEl);
+
             $("#todayWeather").append(oneDayDiv);
         });
 
@@ -68,9 +68,9 @@ $("#search").on("click", function (event) {
     var city = $("#enter-city").val().trim();
     cities.push(city);
     renderCities();
-    showOneDay();
+    showOneDay(city);
 
-// on-click event that when used presses one of the created buttons, weather data is shown.
-$(document).on("click", ".oneDay", showOneDay)
+    // on-click event that when used presses one of the created buttons, weather data is shown.
+    // $(document).on("click", ".oneDay", showOneDay)
 });
 
